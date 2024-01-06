1bb5d951ca7543a7a89df892c5863014ef730a38
Christian Eddie <122130586+Eddie1288@users.noreply.github.com>
2024-01-06 15:33:06 -0700

    Worked on Event List.

--- app/src/main/java/com/example/presentpal/MainActivity.java
+++ app/src/main/java/com/example/presentpal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.presentpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,87 @@ public class MainActivity extends AppCompatActivity {
         birthday.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Birthday.class));
+            }
+        });
 
+        Button christmas = findViewById(R.id.christmas);
+        christmas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Christmas.class));
+            }
+        });
+
+        Button wedding = findViewById(R.id.wedding);
+        wedding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Wedding.class));
+            }
+        });
+
+        Button anniversary = findViewById(R.id.anniversary);
+        anniversary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Anniversary.class));
+            }
+        });
+
+        Button babyShower = findViewById(R.id.babyShower);
+        babyShower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BabyShower.class));
+            }
+        });
+
+        Button mothersDay = findViewById(R.id.mothersDay);
+        mothersDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MothersDay.class));
+            }
+        });
+
+        Button fathersDay = findViewById(R.id.fathersDay);
+        fathersDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FathersDay.class));
+            }
+        });
+
+        Button houseWarming = findViewById(R.id.houseWarming);
+        houseWarming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HouseWarming.class));
+            }
+        });
+
+        Button date = findViewById(R.id.date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Date.class));
+            }
+        });
+
+        Button shoppingList = findViewById(R.id.shoppingList);
+        shoppingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ShoppingList.class));
+            }
+        });
+
+        Button customEvent = findViewById(R.id.customEvent);
+        customEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CustomEvent.class));
             }
         });
     }
